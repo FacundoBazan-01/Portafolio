3104ecb396524812d19b4ddfe057be9e44d47c1e
unknown <facubazan992009@gmail.com>
2025-04-22 21:37:35 -0300

    Agrego foto

--- trabajoPractico1-portafolio/src/components/Home.jsx
+++ trabajoPractico1-portafolio/src/components/Home.jsx
@@ -3,14 +3,14 @@ import Header from './Header'
 import Footer from './Footer'
 import Main from './Main'
 import "../styles/Home.css"
-
+import reactLogo from '../assets/IMG-20240920-WA0062.jpg'
 
 const Home = () => {
     const Alumno = {
         nombre: "Facundo",
         apellido: "Bazan",
         edad: 26,
-       /*  img: <img src={reactLogo} /> , */
+        img: `${reactLogo}`,
         email: "Facubazan992009@gmail.com",
         ocupacion: "Estudiante de programación de la facultad regional Tucumán ",
         telefono: 3816281616,
